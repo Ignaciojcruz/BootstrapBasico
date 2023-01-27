137f19d9e18b142c31e2ca32742c4301fcb8b17c
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-01-27 20:43:22 -0300

    add desagregado por vistas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,81 @@ namespace IntroduccionABootstrap.Controllers
             return View();
         }
 
+        public IActionResult Alineacion()
+        {
+            return View();
+        }
+
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
+        public IActionResult Gutter()
+        {
+            return View();
+        }
+
+        public IActionResult Offset() 
+        {
+            return View();
+        }
+
+        public IActionResult Imagenes() 
+        {
+            return View();
+        }
+
+        public IActionResult Colores()
+        {
+            return View();
+        }
+
+        public IActionResult Tablas()
+        {
+            return View();
+        }
+
+        public IActionResult Video() 
+        {
+            return View();
+        }
+
+        public IActionResult Sizes()
+        {
+            return View();
+        }
+
+        public IActionResult Margenes() 
+        {
+            return View();
+        }
+
+        public IActionResult Textos()
+        {
+            return View();
+        }
+
+        public IActionResult Formulario() 
+        {
+            return View();
+        }
+
+        public IActionResult Acordeon() 
+        {
+            return View();
+        }
+
+        public IActionResult Alertas() 
+        {
+            return View();
+        }
+
+        public IActionResult Carousel() 
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
